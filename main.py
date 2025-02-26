d040282b434d84dcb831257bbc4248386aae79c9
Madhusmita Mohapatra <madhusmita.mohapatra2416@gmail.com>
2025-02-26 17:18:29 -0500

    Fifth commit

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 number_one = 24
 number_two = 12
+word = "python"
---
+++ reverse_a_string.py
@@ -0,0 +1,5 @@
+from main import word
+
+reverse_a_string = ''.join(reversed(word))
+
+print(f'reverse a string "{word}" is {reverse_a_string}')
