ebba2f0d245d9159a3affe8167280bec29434221
Madhusmita Mohapatra <madhusmita.mohapatra2416@gmail.com>
2025-02-26 16:34:30 -0500

    "Second commit"

---
+++ addition.py
@@ -0,0 +1,4 @@
+from main import number_one
+from main import number_two
+addition_of_two_number = number_one +number_two
+print(f' Addition of two numbers:- {number_one}+{number_two}={addition_of_two_number}')
---
+++ subtraction.py
@@ -0,0 +1,6 @@
+from main import number_one
+from main import number_two
+
+subtraction_of_two_number = number_one - number_two
+
+print(f'Subtraction of two numbers:- {number_one}-{number_two} = {subtraction_of_two_number}')
