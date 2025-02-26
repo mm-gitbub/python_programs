b148efb842436cfd850e98d208d4c5c75a896125
Madhusmita Mohapatra <madhusmita.mohapatra2416@gmail.com>
2025-02-26 16:49:48 -0500

    "Third commit"

---
+++ multiplication.py
@@ -0,0 +1,6 @@
+from main import number_one
+from main import number_two
+
+multiplication_of_two_numbers = number_one * number_two
+
+print(f'Multiplication of two numbers:- {number_one}*{number_two}={multiplication_of_two_numbers}')
