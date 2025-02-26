5aef7e67d108230e40770a61ae41c537c197f12c
Madhusmita Mohapatra <madhusmita.mohapatra2416@gmail.com>
2025-02-26 17:11:21 -0500

    Forth commit

---
+++ division.py
@@ -0,0 +1,6 @@
+from main import number_one
+from main import number_two
+
+division_of_two_numbers = number_one/number_two
+
+print(f'Division of two numbers:- {number_one}/{number_two} = {division_of_two_numbers}')
